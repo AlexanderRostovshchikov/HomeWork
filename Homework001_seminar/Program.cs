462fafdf9d5581fb60f8d00d2e300e793fccda65
Alexander <ad-87@inbox.ru>
2022-08-11 14:11:04 +0300

    Сделал дз по семинару#

---
+++ Homework001_seminar/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = 10;
+int b = 7;
+int max = a;
+int min;
+if (b > max) {
+    max = b;
+    min = a;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.Write(min);
+} else {
+    min = b;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.Write(min);
+}
---
+++ Homework004_seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 0) {
+    for (int b = 1; b <= a; b++) {
+        if (b%2 == 0) {
+            Console.Write($"{b} ");
+            if(a - 1 > b) {
+                Console.Write(",");
+            }
+        } 
+    }
+} else Console.Write("Число должно быть больше нуля"); 
+
